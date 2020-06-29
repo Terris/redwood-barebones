79a1d36c8ef1ae358848fb36d5e7024816c7fc5f
Terris Kremer <terris.kremer@gmail.com>
2020-06-29 17:07:55 -0400

    1: setup

--- web/src/index.js
+++ web/src/index.js
@@ -1,16 +1,24 @@
 import ReactDOM from 'react-dom'
+import { AuthProvider } from '@redwoodjs/auth'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
+import GoTrue from 'gotrue-js'
 
 import Routes from 'src/Routes'
-
 import './index.css'
+
+const goTrueInstance = new GoTrue({
+  APIUrl: 'https://MYAPP.netlify.app/.netlify/identity',
+  setCookie: true,
+})
 
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider>
-      <Routes />
-    </RedwoodProvider>
+    <AuthProvider client={goTrueInstance} type="goTrue">
+      <RedwoodProvider>
+        <Routes />
+      </RedwoodProvider>
+    </AuthProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
 )
